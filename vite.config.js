0d27aa5c4a70ab2a5c1ef8648cbd6c68f0650884
achintya <achintya623@gmail.com>
2025-10-06 18:54:55 +0530

    Final project structure + Vite build ready

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CRITICAL FIX: Setting the base forces Rollup to correctly handle absolute paths in the build output.
-  base: "./",
-  root: "./",
-  build: {
-    outDir: "dist",
-  },
 });
